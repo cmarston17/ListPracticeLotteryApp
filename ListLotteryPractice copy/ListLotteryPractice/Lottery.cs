c3fdc6d2d06c97dc107b9f1eba3c1f4d2e6ffa92
CMarston17 <cmarston17@uc.apu.edu>
2017-11-07 05:52:03 -0800

    Lists in c# lottery application
    Practicing using lists in c# with a lottery app!

---
+++ ListLotteryPractice copy/ListLotteryPractice/Lottery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListLotteryPractice
+{
+    public class Lottery
+    {
+        private List<int> numbers;
+        private Random random;
+
+        public Lottery()
+        {
+            random = new Random();
+            numbers = new List<int>();
+        }
+
+        public int Lot()
+        {
+            int number = random.Next(100) + 1;
+            numbers.Add(number);
+            return number;
+        }
+
+        public string print()
+        {
+            string s = "";
+            numbers.Sort();
+            foreach (int i in numbers)
+                s += i + " ";
+            return s;
+
+        }
+    }
+}
---
+++ ListLotteryPractice copy/ListLotteryPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListLotteryPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lottery lottery = new Lottery();
+            Console.WriteLine("Welcome to the lottery program!");
+            char choice = '0';
+            // main loop
+            while (choice != 3)
+            {
+                //options list
+                Console.WriteLine("1 - Lot the next number please!");
+                Console.WriteLine("2 - Print the list please!");
+                Console.WriteLine("3 - Quit the program!");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                // reaction to the choice
+                switch (choice)
+                {
+                    case '1':
+                        Console.WriteLine("You got a: {0}", lottery.Lot());
+                        break;
+                    case '2':
+                        Console.WriteLine("Numbers Drawn: {0}", lottery.print());
+                        break;
+                    case '3':
+                        Console.WriteLine("Thanks for using the lottery program!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again!");
+                        break;
+                }
+            }
+        }
+    }
+}
